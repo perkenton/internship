277e682d91b64c30c5d2a229fbde1eea1387197a
Alexander Vasiliev <selado@ya.ru>
2022-03-04 01:00:59 +0500

    V-01: добавлен и стилизован компонент бокового меню

---
+++ src/common/src/components/AsideMenu/AsideMenu.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import styles from './AsideMenu.module.scss';
+import { MenuOutlined, PlusOutlined } from '@ant-design/icons';
+import cn from 'classnames';
+import TelegramIcon from '../../assets/icons/Telegram/Telegram';
+import FacebookIcon from '../../assets/icons/Facebook/Facebook';
+import InstagramIcon from '../../assets/icons/Instagram/Instagram';
+import { Lang } from '../../model/model';
+
+
+export default function AsideMenu() {
+  const [ isOpened, setIsOpened ] = useState<boolean>(false);
+  const [ lang, setLang ] = useState<Lang>(Lang.Ru);
+  const LANG_MAP = {
+    'Ru': 'Eng',
+    'En': 'Рус',
+  };
+  const asideClass = cn(styles.asideBlock, isOpened && styles.asideBlockOpened);
+  const menuBlockClass = cn(styles.menuBlock, isOpened && styles.menuBlockOpened);
+
+
+  function toggleCollapsed() {
+    setIsOpened(!isOpened);
+  }
+
+  function changeLang() {
+    switch(lang) {
+      case Lang.En:
+        setLang(Lang.Ru);
+        break;
+      case Lang.Ru:
+        setLang(Lang.En);
+        break;
+    }
+  }
+
+
+  return (
+    <aside className={ asideClass }>
+      <div className={ styles.aside }>
+        <div onClick={ toggleCollapsed } className={ styles.burgerButton } >
+          { isOpened ? <PlusOutlined className={ styles.crossIcon }/> : <MenuOutlined className={ styles.burgerIcon }/> }
+        </div>
+        { !isOpened && <p className={ styles.lang } onClick={ changeLang }>{ LANG_MAP[lang] }</p> }
+      </div>
+
+      <div className={ menuBlockClass }>
+        <ul className={ styles.menuList }>
+          <li className={ styles.menuItem }><a href='#' className={ styles.menuItemLink}>Парковка</a></li>
+          <li className={ styles.menuItem }><a href='#' className={ styles.menuItemLink}>Cтраховка</a></li>
+          <li className={ styles.menuItem }><a href='#' className={ styles.menuItemLink}>Бензин</a></li>
+          <li className={ styles.menuItem }><a href='#' className={ styles.menuItemLink}>Обслуживание</a></li>
+        </ul>
+        <div className={ styles.iconsBlock }>
+          <a href='https://telegram.org' className={ styles.iconLink } target='_blank' rel='noreferrer'><TelegramIcon /></a>
+          <a href='https://www.facebook.com' className={ styles.iconLink } target='_blank' rel='noreferrer'><FacebookIcon /></a>
+          <a href='https://www.instagram.com' className={ styles.iconLink } target='_blank' rel='noreferrer'><InstagramIcon /></a>
+        </div>
+      </div>
+    </aside>
+  )
+}

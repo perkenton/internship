60053ff68a5f40c2d1bd546676efd576517d9580
Alexander Vasiliev <selado@ya.ru>
2022-03-06 19:43:20 +0500

    V-01: настроен компонент главной страницы

---
+++ src/components/MainPage/MainPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './MainPage.module.scss';
+import AsideMenu from '../../common/src/components/AsideMenu/AsideMenu';
+import MainBlock from './MainBlock/MainBlock';
+import CarouselBlock from './CarouselBlock/CarouselBlock';
+
+
+export default function MainPage() {
+
+  return (
+    <div className={ styles.mainPage }>
+      <AsideMenu />
+      <MainBlock />
+      <CarouselBlock />
+    </div>
+  )
+}

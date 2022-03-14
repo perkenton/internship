070e82236577fdcbe4e6a1f88a8481be202e1ca4
Alexander Vasiliev <selado@ya.ru>
2022-03-14 21:14:56 +0500

    F-01: данные для слайдера вынесены в отдельный файл

--- src/components/MainPage/CarouselBlock/CarouselBlock.tsx
+++ src/components/MainPage/CarouselBlock/CarouselBlock.tsx
@@ -4,51 +4,8 @@ import cn from 'classnames';
 import { Carousel } from 'antd';
 import { CarouselRef } from 'antd/es/carousel';
 import { LeftOutlined, RightOutlined } from '@ant-design/icons';
-import { NBS } from '../../../common/src/constants';
-import CustomButton, { ButtonColor } from '../../../common/src/components/CustomButton/CustomButton';
-import photoOne from './assets/jp-valery-1052513-unsplash-1.jpg';
-import photoTwo from './assets/jp-valery-1052513-unsplash-2.jpg';
-import photoThree from './assets/jp-valery-1052513-unsplash-3.jpg';
-import photoFour from './assets/jp-valery-1052513-unsplash-4.jpg';
-
-type CarouselData = {
-  id: number,
-  image: string,
-  title: string,
-  subtitle: string,
-  buttonColor: ButtonColor,
-}
-
-const CAROUSEL_DATA: CarouselData[] = [
-  {
-    id: 1,
-    image: photoOne,
-    title: 'Бесплатная парковка',
-    subtitle: `Оставляйте машину на${NBS}платных городских парковках и${NBS}разрешенных местах, не${NBS}нарушая ПДД, а${NBS}также в${NBS}аэропортах`,
-    buttonColor: 'green',
-  },
-  {
-    id: 2,
-    image: photoTwo,
-    title: 'Страховка',
-    subtitle: `Полная страховка автомобиля`,
-    buttonColor: 'blue',
-  },
-  {
-    id: 3,
-    image: photoThree,
-    title: 'Бензин',
-    subtitle: `Полный бак на${NBS}любой заправке города за${NBS}наш счёт`,
-    buttonColor: 'red',
-  },
-  {
-    id: 4,
-    image: photoFour,
-    title: 'Обслуживание',
-    subtitle: `Автомобиль проходит еженедельное${NBS}ТО`,
-    buttonColor: 'purple',
-  },
-]
+import CustomButton from '../../../common/src/components/CustomButton/CustomButton';
+import { CAROUSEL_DATA, CarouselData } from './constants';
 
 
 export default function CarouselBlock() {

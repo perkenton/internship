e11512ef4c717d03423b5ab76b2e24887addbc27
Alexander Vasiliev <selado@ya.ru>
2022-03-20 04:23:42 +0500

    F-01: добавлен пропс дял передачи класса

--- src/common/src/components/Header/Header.tsx
+++ src/common/src/components/Header/Header.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import styles from './Header.module.scss';
+import cn from 'classnames';
 import Placemark from '../../assets/icons/Placemark/Placemark';
 
+type HeaderType = {
+  classes?: string,
+}
 
-export default function Header() {
+export default function Header({ classes }: HeaderType) {
+  const headerClass = cn(styles.header, classes);
 
   return (
-    <header className={ styles.header }>
+    <header className={ headerClass }>
       <p className={ styles.logo }>Need for drive</p>
       <div className={ styles.cityBlock }>
         <span className={ styles.placemarkIcon }><Placemark /></span>

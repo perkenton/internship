9759a901365f6d339f79b54ca81bbde97eac7196
Alexander Vasiliev <selado@ya.ru>
2022-03-22 02:32:48 +0500

    V-02: настроена ссылка на логотипе в Header

--- src/common/src/components/Header/Header.tsx
+++ src/common/src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 import cn from 'classnames';
 import Placemark from '../../assets/icons/Placemark/Placemark';
@@ -12,7 +13,9 @@ export default function Header({ classes }: HeaderType) {
 
   return (
     <header className={ headerClass }>
-      <p className={ styles.logo }>Need for drive</p>
+      <Link to='/'>
+        <p className={ styles.logo }>Need for drive</p>
+      </Link>
       <div className={ styles.cityBlock }>
         <span className={ styles.placemarkIcon }><Placemark /></span>
         <span className={ styles.cityText }>Ульяновск</span>
